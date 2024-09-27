0ea984d69dacc78462ee3cdb234a5d1682e869d6
Mihai Vasiliu <mihai.vasiliu.93@gmail.com>
2024-09-27 16:20:36 +0300

    Move port in VLAN 1 if no VLAN information is send by Radius
    Signed-off-by: Mihai Vasiliu <mihai.vasiliu.93@gmail.com>

--- src/ap/ieee802_1x.c
+++ src/ap/ieee802_1x.c
@@ -2126,7 +2126,11 @@ ieee802_1x_receive_auth(struct radius_msg *msg, struct radius_msg *req,
 							vlan_desc.tagged);
 		sta->vlan_id = vlan_desc.untagged;
 
-		if (sta->vlan_id > 0) {
+		if (sta->vlan_id >= 0) {
+			if (sta->vlan_id == 0) {
+				wpa_printf(MSG_DEBUG, "MAB: No VLAN information received from RADIUS!");
+				sta->vlan_id = 1; //put in VLAN 1
+			}
 			const char *br_name;
 			br_name = hostapd_get_vlan_id_ifname(hapd->conf->vlan, sta->vlan_id);
 			if (br_name) {
@@ -2136,7 +2140,7 @@ ieee802_1x_receive_auth(struct radius_msg *msg, struct radius_msg *req,
 				wpa_printf(MSG_ERROR, "MAB: No bridge configured for VLAN %d in the vlan_file!", sta->vlan_id);
 			}
 		} else {
-			wpa_printf(MSG_DEBUG, "MAB: No VLAN information received from RADIUS! Port will be left in the mab_bridge!");
+			wpa_printf(MSG_DEBUG, "MAB: Error getting VLAN ID!");
 		}
 #endif /* CONFIG_ENABLE_MAB */
 #endif /* CONFIG_NO_VLAN */
